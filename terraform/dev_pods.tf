94861010db510e93caf9802946d8b4dbb15c7e43
NadeenAbdElazimAZ <nadeen.abdelazim.ibrahim@gmail.com>
2024-06-25 01:13:12 +0200

    Create two pods (one for nodejs application and another for MySQL DB ) in dev namespace

---
+++ terraform/dev_pods.tf
@@ -0,0 +1,55 @@
+resource "kubernetes_pod" "nodejs-app" {
+  metadata {
+    name      = "nodejs-app"
+    namespace = "dev"
+  }
+  spec {
+    container {
+      image = "node:14"
+      name  = "nodejs-app"
+      port {
+        container_port = 5000
+      }
+      env {
+        name  = "MYSQL_HOST"
+        value = "mysql"
+      }
+      env {
+        name  = "MYSQL_USER"
+        value = "root"
+      }
+      env {
+        name  = "MYSQL_PASSWORD"
+        value = "12345"
+      }
+      env {
+        name  = "MYSQL_DB"
+        value = "mydatabase"
+      }
+    }
+  }
+}
+
+resource "kubernetes_pod" "mysql" {
+  metadata {
+    name      = "mysql"
+    namespace = "dev"
+  }
+  spec {
+    container {
+      image = "mysql:5.7"
+      name  = "mysql"
+      env {
+        name  = "MYSQL_ROOT_PASSWORD"
+        value = "12345"
+      }
+      env {
+        name  = "MYSQL_DATABASE"
+        value = "mydatabase"
+      }
+      port {
+        container_port = 5500
+      }
+    }
+  }
+}
