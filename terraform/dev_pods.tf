f18df58ef84f4a16b0b89bac6c4f0f620ca74ee9
NadeenAbdElazimAZ <nadeen.abdelazim.ibrahim@gmail.com>
2024-06-28 05:25:11 +0200

    Delete app_pod

--- terraform/dev_pods.tf
+++ terraform/dev_pods.tf
@@ -1,36 +1,3 @@
-
-resource "kubernetes_pod" "nodejs-app" {
-  metadata {
-    name      = "nodejs-app"
-    namespace = kubernetes_namespace.dev.metadata[0].name
-  }
-  spec {
-    container {
-      image = "node:14"
-      name  = "nodejs-app"
-      port {
-        container_port = 5000
-      }
-      env {
-        name  = "MYSQL_HOST"
-        value = "mysql"
-      }
-      env {
-        name  = "MYSQL_USER"
-        value = "root"
-      }
-      env {
-        name  = "MYSQL_PASSWORD"
-        value = "12345"
-      }
-      env {
-        name  = "MYSQL_DB"
-        value = "mydatabase"
-      }
-    }
-  }
-}
-
 resource "kubernetes_pod" "mysql" {
   metadata {
     name      = "mysql"
