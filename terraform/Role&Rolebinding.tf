e5390f1a36553fac165e2913747e3e7e0ea3d885
NadeenAbdElazimAZ <nadeen.abdelazim.ibrahim@gmail.com>
2024-06-28 05:22:48 +0200

    addd Role&Rolebinding

---
+++ terraform/Role&Rolebinding.tf
@@ -0,0 +1,56 @@
+
+
+# Create the Role with specified rules
+resource "kubernetes_role" "jenkins_role" {
+  metadata {
+    name      = "jenkins"
+    namespace = "dev"
+    labels = {
+      "app.kubernetes.io/name" = "jenkins"
+    }
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods"]
+    verbs      = ["create", "delete", "get", "list", "patch", "update", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods/exec"]
+    verbs      = ["create", "delete", "get", "list", "patch", "update", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods/log"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["secrets"]
+    verbs      = ["get"]
+  }
+}
+
+# Create the RoleBinding
+resource "kubernetes_role_binding" "jenkins_role_binding" {
+  metadata {
+    name      = "jenkins-role-binding"
+    namespace = "dev"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.jenkins_role.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.jenkins_admin.metadata[0].name
+    namespace = "tools"
+  }
+}
